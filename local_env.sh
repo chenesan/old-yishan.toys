83ab1b1c57ccd6966b2b7cc5fd4881b3a4984b3f
陳乙山 <pipio1994@gmail.com>
2017-04-05 19:29:50 +0800

    Prevent verbose --settings when using manage.py

---
+++ local_env.sh
@@ -0,0 +1,3 @@
+export DJANGO_SETTINGS_MODULE='loperuv.settings.local'
+export DJANGO_SECRET_KEY=''
+export GOOGLE_ANALYTICS_PROPERTY_ID=''
--- production_env.sh
+++ production_env.sh
@@ -1,2 +1,3 @@
+export DJANGO_SETTINGS_MODULE='loperuv.settings.production'
 export DJANGO_SECRET_KEY=''
 export GOOGLE_ANALYTICS_PROPERTY_ID=''
