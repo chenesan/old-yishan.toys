04667e79759f663592579552a51304e68ef32080
陳乙山 <pipio1994@gmail.com>
2017-04-06 10:51:50 +0800

    Add private production config

---
+++ my_local_env.sh
@@ -0,0 +1,5 @@
+export DJANGO_SETTINGS_MODULE='loperuv.settings.local'
+export DJANGO_SECRET_KEY='f#^r2-5+08xbofx!o&dfhs1uqwok3-a!1**j8g#39-u=)kzgw4'
+export GOOGLE_ANALYTICS_PROPERTY_ID=UA-75310497-1
+export DISQUS_API_KEY='4d32ee347147444c840fe3e791bab12d'
+export DISQUS_WEBSITE_SHORTNAME='wwwloperuvcom'
