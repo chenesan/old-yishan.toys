8843e18d4833e7592246343f59afd8ac9202fbf5
陳乙山 <pipio1994@gmail.com>
2017-04-06 10:35:04 +0800

    Move disqus variable to env.sh

--- local_env.sh
+++ local_env.sh
@@ -1,3 +1,5 @@
 export DJANGO_SETTINGS_MODULE='loperuv.settings.local'
 export DJANGO_SECRET_KEY=''
 export GOOGLE_ANALYTICS_PROPERTY_ID=''
+export DISQUS_API_KEY=''
+export DISQUS_WEBSITE_SHORTNAME=''
--- production_env.sh
+++ production_env.sh
@@ -1,3 +1,5 @@
 export DJANGO_SETTINGS_MODULE='loperuv.settings.production'
 export DJANGO_SECRET_KEY=''
 export GOOGLE_ANALYTICS_PROPERTY_ID=''
+export DISQUS_API_KEY=''
+export DISQUS_WEBSITE_SHORTNAME=''
